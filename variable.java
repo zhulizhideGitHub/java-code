6a544122ee2cbb4493a23e4a71c88de4f94e21ff
zhulizhideGitHub <18506571953@163.com>
2022-08-18 17:12:06 +0800

    variable

---
+++ variable.java
@@ -0,0 +1,10 @@
+public class variable {
+    public static void main(String[] args) {
+        int i = 10;
+        System.out.println(i);
+        //int i = 20;同一个作用域中，变量名不能重复重名，变量可以重新赋值
+        i = 30;// i 重新赋值
+        System.out.println(i);
+
+    }
+}
